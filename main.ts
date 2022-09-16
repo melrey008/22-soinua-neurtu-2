a8a88a69cea9746f1ef9db5a80b48e253440794d
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-09-16 11:11:13 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+if (input.soundLevel() < 0) {
+	
+}
+if (input.soundLevel() < 0) {
+	
+}
+if (input.soundLevel() > 0) {
+	
+}
+if (input.soundLevel() > 0) {
+	
+}
+if (input.soundLevel() > 0) {
+	
+}
