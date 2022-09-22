d74f452150d2a32a865824ea60846957359b2717
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-09-22 12:53:43 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,45 @@
-if (input.soundLevel() < 0) {
-	
+if (input.soundLevel() < 25) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
 }
-if (input.soundLevel() < 0) {
-	
+if (input.soundLevel() < 50) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
 }
-if (input.soundLevel() > 0) {
-	
+if (input.soundLevel() < 100) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 }
-if (input.soundLevel() > 0) {
-	
+if (input.soundLevel() < 150) {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 }
-if (input.soundLevel() > 0) {
-	
+if (input.soundLevel() > 200) {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 }
